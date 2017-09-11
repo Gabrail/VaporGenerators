5477f592dd9b6462c8d4e6c6d7cb038d502440f5
jeremybarger <jeremyjusticebarger@gmail.com>
2017-09-11 13:35:12 -0600

    update to swift 4.0 package api

--- Package.swift
+++ Package.swift
@@ -1,11 +1,15 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "VaporGenerators",
+    products: [],
     dependencies: [
         .Package(url: "https://github.com/vapor/console.git", majorVersion: 1, minor: 0)
-    ],
-    exclude: [
-        "Templates"
+    ]
+    targets: [
+        .target(name: "App"),
+        .target(name: "Run", dependencies: ["App"]),
+
     ]
 )
